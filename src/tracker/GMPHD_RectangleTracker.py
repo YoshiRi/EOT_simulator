35b3f0d308e65d24da1bd39e55b82e7ce5187e2e
Yoshi Ri <yoshi.ri@tier4.jp>
2022-08-19 09:44:15 +0900

    add rectangle division func for prediction part

--- src/tracker/GMPHD_RectangleTracker.py
+++ src/tracker/GMPHD_RectangleTracker.py
@@ -19,7 +19,7 @@ import numpy as np
 import itertools
 from enum import Enum
 
-from utils import rot_mat_2d
+from utils import rot_mat_2d, vec2rad, rad_distance
 
 
 
@@ -103,3 +103,111 @@ def find_corner_index(measurements):
             dmin = d
     return corner_index
 
+class RectangleShapePrediction():
+    def __init__(self) -> None:
+        self.orientation = None
+        self.center = None
+        self.width = None
+        self.length = None
+
+    def normal_vector_angles(self):
+        """Return Normal vector angles
+
+        front
+
+        +--b1---+
+        |       |
+        |       |
+        b4  ^   b2
+        |   |   |
+        |       |
+        |       |
+        +---b3--+
+
+        Returns:
+            list of normal vector angle : [b1, b2, b3, b4]
+        """
+        n_vecs = [self.orientation + np.pi, self.orientation + np.pi/2, self.orientation, self.orientation - np.pi/2]
+        return n_vecs
+    
+    def find_closest_angle(self,angle_rad, threshold=0.5):
+        """find closest direction
+
+        Args:
+            angle_rad (_type_): _description_
+            threshold (float, optional): direction error . Defaults to 0.5 (28.6degree)
+
+        Returns:
+            int : closest direction index
+        """
+        angles = self.normal_vector_angles()
+
+        for i in range(4):
+            drad = rad_distance(angles[i], angle_rad)
+            if drad < threshold:
+                return i
+        # if no closest point 
+        return None
+
+    def get_equally_divided_coords(self,division_num,w,l):
+        """divide one side
+
+        Args:
+            division_num (_type_): _description_
+            w (_type_): width
+            l (_type_): length
+
+        
+        division_num
+        ---o-----o-----o-----o---- 
+                                    ^
+                                    |
+                                    |
+                                    |
+                                    | length/2
+                   center           |
+                                    |
+                     x              v
+        <---------------------------->
+                    width
+
+        Returns:
+            _type_: coordinates for divided points
+        """
+        divided_coords = []
+        y = l/2
+        x0 = - w/2
+        for i in range(division_num):
+            dx = (2*i + 1) / division_num/ 2 * w
+            divided_coords.append([x0 + dx, y])
+        return np.array(divided_coords).reshape(-1,2)
+
+    def divide_coords(self, div_num, indx):
+        """Assume lidar estimation is clock wise
+
+        Args:
+            div_num (_type_): number of measurement point in one side
+            indx (_type_): side index 0 is front, 1 is right, 2 is back, 3 is left 
+
+        Returns:
+            _type_: Nx2 np array [[x1,y1],[x2, y2]...]
+        """
+        R90 = rot_mat_2d(np.pi/2)
+        R180 = rot_mat_2d(np.pi)
+        center = np.array(self.center).reshape(-1,2) 
+        
+        if indx == 0:
+            coords_ = self.get_equally_divided_coords(div_num,self.width,self.length)
+            coords = center + coords_
+        elif indx == 1:
+            coords_ = self.get_equally_divided_coords(div_num,self.length,self.width)
+            coords = np.transpose(R90.T @ coords_.T)
+        elif indx == 2:
+            coords_ = self.get_equally_divided_coords(div_num,self.width,self.length)
+            coords = np.transpose(R180 @ coords_.T)
+        elif indx == 3:
+            coords_ = self.get_equally_divided_coords(div_num,self.length,self.width)
+            coords = np.transpose(R90 @ coords_.T)
+        else:
+            coords =  None
+        return coords
--- tests/test_GMPHD_RectangleTracker.py
+++ tests/test_GMPHD_RectangleTracker.py
@@ -17,3 +17,18 @@ def test_point2line_distance():
 def test_find_corner_index():
     z_1 = [[0,1],[0,0],[1,0],[2,0],[3,0]]
     assert find_corner_index(z_1) == 1
+
+
+def test_coords_divide():
+    rsp = RectangleShapePrediction()
+    rsp.center = [0,0]
+    rsp.width = 4
+    rsp.length = 10
+
+    front = rsp.divide_coords(1,0)[0]
+    estimated_front = np.array([0,rsp.length/2])
+    assert np.allclose(front,estimated_front)
+
+    right = rsp.divide_coords(3,1)[1]         # take center point
+    estimated_right = np.array([rsp.width/2,0])
+    assert np.allclose(right,estimated_right) # array_equal sometimes fail due to calc error
