5a6744b18c37b0a0e847423f0c87e9de6b77ff15
Yoshi Ri <yoshi.ri@tier4.jp>
2022-08-19 09:43:12 +0900

    add rad_distance to utils

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,12 @@ def rot_mat_2d(theta):
     s = np.sin(theta)
     c = np.cos(theta)
     return np.array([[c,-s],[s,c]]).reshape(2,2)
+
+def vec2rad(v):
+    return np.arctan2(v[1],v[0])
+
+def rad_distance(a1,a2):
+    dist = np.abs(a1 - a2)
+    while dist >= 2*np.pi:
+        dist -= 2*np.pi
+    return dist
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,3 +4,7 @@ from src.utils import *
 def test_rot_mat_2d():
     vec = np.array([3,4]).reshape(-1,1)
     assert np.array_equal(vec,rot_mat_2d(0) @ vec)
+
+def test_rad_distance():
+    assert rad_distance(0,2*np.pi) == 0
+    assert rad_distance(0,-5*np.pi) == np.pi
