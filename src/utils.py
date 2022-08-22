e6e52b46d005b313de45093fc93fe90b782bea6b
Yoshi Ri <yoshi.ri@tier4.jp>
2022-08-22 13:43:52 +0900

    add numerical derivative function to util

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 
 def rot_mat_2d(theta):
     s = np.sin(theta)
@@ -13,3 +14,18 @@ def rad_distance(a1,a2):
     while dist >= 2*np.pi:
         dist -= 2*np.pi
     return dist
+
+
+# numerical gradient input: func must be a function and x is vector 
+def numerical_grad(func, x, i, eps=1e-9, *other_args):
+    x_arr = np.array(x,dtype=float).reshape(-1)
+    x_p, x_m = deepcopy(x_arr), deepcopy(x_arr)
+    r_eps = max(np.abs(x_m[i])*eps, eps)
+    x_m[i] -= r_eps
+    x_p[i] += r_eps
+    print(x_arr, x_m, x_p)
+    fp = func(x_p, *other_args)
+    fm = func(x_m, *other_args)
+    grad = (fp-fm)/r_eps/2
+    return grad
+
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,3 +8,12 @@ def test_rot_mat_2d():
 def test_rad_distance():
     assert rad_distance(0,2*np.pi) == 0
     assert rad_distance(0,-5*np.pi) == np.pi
+
+
+def test_numerical_grad():
+    x_square = lambda x: x*x
+    x0, x1, x2 = np.array([0.0]), np.array([1.0]), np.array([1e9])
+    assert numerical_grad(x_square, x0, 0) == 0
+    assert np.allclose(numerical_grad(x_square, x1, 0) , 2.0*x1)
+    assert numerical_grad(x_square, x2, 0) ==  2.0*x2 
+    
