eaaaa596150dfb3843f9e2e1359c208d9f604e88
Yoshi Ri <yoshi.ri@tier4.jp>
2022-08-23 15:36:09 +0900

    fixed arg handover process

--- src/utils.py
+++ src/utils.py
@@ -17,23 +17,23 @@ def rad_distance(a1,a2):
 
 
 # numerical gradient input: func must be a function and x is vector 
-def numerical_grad(func, x, i, eps=1e-9, *other_args):
+def numerical_grad(func, x, i, eps=1e-9, **other_args):
     x_arr = np.array(x,dtype=float).reshape(-1)
     x_p, x_m = deepcopy(x_arr), deepcopy(x_arr)
     r_eps = max(np.abs(x_m[i])*eps, eps)
     x_m[i] -= r_eps
     x_p[i] += r_eps
-    fp = func(x_p, *other_args)
-    fm = func(x_m, *other_args)
+    fp = func(x_p, **other_args)
+    fm = func(x_m, **other_args)
     grad = (fp-fm)/r_eps/2
     return grad
 
-def numerical_jacob(func, x, *other_args):
+def numerical_jacob(func, x, **other_args):
     x = np.array(x).reshape(-1)
     N = len(x)
     Jacob = np.array([])
     for i in range(N):
-        grad_ = numerical_grad(func,x,i,*other_args)
+        grad_ = numerical_grad(func,x,i,**other_args)
         grad = grad_.reshape(1,-1)
         Jacob = np.vstack([Jacob,grad]) if Jacob.size else grad
     return Jacob
--- tests/test_EKF.py
+++ tests/test_EKF.py
@@ -1,3 +1,5 @@
+from random import sample
+from this import d
 import pytest
 from src.EKF import ExtendedKalmanFilter
 import numpy as np
@@ -7,3 +9,11 @@ def test_EKF():
     x_init = np.array([0.,0.]).reshape(-1,1)
     P_init = np.diag([1e9,1e9])
     ekf.init_state(x_init,P_init)
+
+    def sample_predict(x, y=None, dt=0):
+        x_ = np.copy(x)
+        x_[0] = x_[1]*dt
+        return x_
+
+    Q = np.diag([0.1,0.1])
+    ekf.predict_nonlinear(sample_predict,Q,None,dt=0.1)
