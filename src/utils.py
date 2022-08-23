fix jacobian shape (transposed)

src/utils.py
@@ -34,6 +34,6 @@ def numerical_jacob(func, x, **other_args):
     Jacob = np.array([])
     for i in range(N):
         grad_ = numerical_grad(func,x,i,**other_args)
-        grad = grad_.reshape(1,-1)
-        Jacob = np.vstack([Jacob,grad]) if Jacob.size else grad
+        grad = grad_.reshape(-1,1)
+        Jacob = np.hstack([Jacob,grad]) if Jacob.size else grad
     return Jacob

tests/test_utils.py
@@ -23,7 +23,7 @@ def test_numerical_grad():
 
 def test_numerical_jacob():
     func = lambda x: x[0]*x[0] + np.cos(x[1])
-    d_func = lambda x: np.array( [2*x[0],-np.sin(x[1])]).reshape(-1,1)
+    d_func = lambda x: np.array( [2*x[0],-np.sin(x[1])]).reshape(1,-1)
     X0 = np.array([1.0,0.5]).reshape(-1,1)
 
     J = numerical_jacob(func,X0)